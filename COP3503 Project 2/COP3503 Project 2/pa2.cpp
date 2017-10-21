1eafd3d50f263d78c0705b0168b18e7d22830834
chadwishner <chadwishner@gmail.com>
2017-10-21 00:39:31 -0400

    Finished

--- COP3503 Project 2/COP3503 Project 2/pa2.cpp
+++ COP3503 Project 2/COP3503 Project 2/pa2.cpp
@@ -5,71 +5,115 @@
 #include "pa2.h"
 #include <iostream>
 
-int main(int argc, char **argv) {
+/* main
+ @parameters int argc, char** argv
+ */
+int main(int argc, char** argv) {
+	//create variables
 	int userChoice;
 	string programName;
 	int programSize;
+	
+	//create new linked list and initialize 32 pages
 	linkedList* list = new linkedList();
 	list -> initPages();
 
+	//check to make sure that the user started program with proper
 	if (argc != 2){
 		cout << "Error, ending program" << endl;
 		return 1;
 	}
 	else {
+		//save the argument as a string
 		string bestOrWorst = argv[1];
+		
+		//proper formating
 		cout << "Using " << bestOrWorst << " fit algoritm" << endl;
 
+		//print menu
 		menu();
 
+		//loop so that way the user can keep picking options
 		while (userChoice != 5){
 			cout << "Choice - ";
-
 			cin >> userChoice;
 
+			//check to see if the user entered an int, if not end program
+			if (cin.fail()){
+				cout << "Must enter an int, ending program\n" << endl;
+				return -1;
+			}
 			cout << endl;
 
+			//switch statement for user input
 			switch (userChoice) {
 				case 1:
+					//open program
+					
 					cout << "Program name - ";
 					cin >> programName;
 					cout << endl;
+					
 					cout << "Program size (KB) - ";
 					cin >> programSize;
+					
+					//check to see if the user entered an int, if not end program
+					if (cin.fail()){
+						cout << "Must enter an int, ending program\n" << endl;
+						return -1;
+					}
 					cout << endl;
+					
+					//convert kb to pages
 					programSize = kbToPages(programSize);
 					
+					//check to see if program is already open, if so break and allow user to make a new choice
 					if (list -> checkExistence(programName)){
-						cout << "Error, program already open" << endl;
+						cout << "Error, program already open\n" << endl;
 						break;
 					} else {
+						//insert program
 						list -> insert(programName, programSize, bestOrWorst);
 					}
 					break;
 				case 2:
+					//kill program
+					
 					cout << "Program name - ";
 					cin >> programName;
 
+					//check if program is open, if not allow user to pick new option
 					if (!list -> checkExistence(programName)){
-						cout << "The program is not open" << endl;
+						cout << "The program is not open\n" << endl;
 						break;
+						
+					//kill program
 					} else if (list -> checkExistence(programName)){
+						int numberPages = list -> countPages(programName);
 						list -> kill(programName);
-						cout << "Program " << programName << " successfully killed, " << list -> countPages(programName) << " page(s) reclaimed." <<endl;
+						cout << "Program " << programName << " successfully killed, " << numberPages << " page(s) reclaimed.\n" <<endl;
 						break;
 					}
 				case 3:
+					//count fragments and print with proper formating
+					
 					cout << "There are " << list -> countFrag() << " fragment(s)" << endl;
 					break;
 				case 4:
+					//print memory
+					
 					list -> print();
 					cout << "\n" << endl;
 					break;
 				case 5:
+					//exit program
+					
 					return 0;
 					break;
 				default:
-					cout << "Not a proper choice, ending program" << endl;
+					//allow user to enter a new option if they do not enter an int 1-5
+					
+					cout << "Not a proper choice, please enter an option 1-5\n" << endl;
 					break;
 			}
 		}
--- COP3503 Project 2/COP3503 Project 2/pa2.h
+++ COP3503 Project 2/COP3503 Project 2/pa2.h
@@ -6,33 +6,55 @@
 #include <string>
 using namespace std;
 
+//declare functions
 void menu();
 int kbToPages(int programSize);
 
+/* print function
+ @return void
+ */
 void menu(){
 	cout << "\n\t1. Add Program\n\t2. Kill Program\n\t3. Fragmentation\n\t4. Print Memory\n\t5. Exit\n" << endl;
 }
 
+/* kb to pages function
+ @parameter int programSize
+ @return int programPage
+ */
 int kbToPages(int programSize){
+	
+	//divide int by 4 because 4kb per page
 	int programPage = programSize / 4;
+	
+	//check if there is a remainder, if so, increment programPage
 	if (programSize % 4 != 0){
 		programPage++;
 	}
 	return programPage;
 }
 
+/* struct nodePage
+ includes string name
+ node* nextNodePage
+ 
+ */
 struct nodePage{
 	string name;
 	
 	nodePage* nextNodePage;
 	
+	//allow access to node name
 	nodePage(string name){
 		this->name = name;
 	}
 };
 
+/* linked list class
+ */
 class linkedList{
 	private:	nodePage* head;
+	
+	//declare functions
 	public:		linkedList();
 				void initPages();
 				void print();
@@ -44,13 +66,21 @@ class linkedList{
 	
 };
 
+/* initialize linkedlist
+ */
 linkedList::linkedList(){
 	head = NULL;
 }
 
+/* initialize 32 pages
+ @return void
+ */
 void linkedList::initPages(){
+	
+	//create a temp node
 	nodePage* temp = NULL;
 	
+	//create 32 nodes with "free" for name
 	for (int x = 0; x < 32; x++){
 		if (head == NULL){
 			head = new nodePage("free");
@@ -62,9 +92,14 @@ void linkedList::initPages(){
 	}
 }
 
+/* print linked list with proper formating
+ @return void
+ */
 void linkedList::print(){
 	nodePage* temp = head;
 	while (temp != NULL){
+		
+		//proper formatting
 		for (int x = 0; x < 8; x++){
 			if (temp -> name == "free"){
 				cout << temp -> name << "\t";
@@ -77,11 +112,18 @@ void linkedList::print(){
 	}
 }
 
+/* insert function
+ @parameter string name, int programSize, string bestOrWorst
+ @return void
+ */
 void linkedList::insert(string name, int programSize, string bestOrWorst){
+	
+	//initialize variables
 	int currentFrag = 0;
 	int bestFrag = 0;
 	nodePage* startNodeSmallest = NULL;
 	nodePage* startNodeCurrent = NULL;
+	
 	if(head == NULL){
 		head = new nodePage(name);
 		return;
@@ -89,9 +131,16 @@ void linkedList::insert(string name, int programSize, string bestOrWorst){
 
 	nodePage * temp = head;
 
+	//if user wanted best algorithm
 	if(bestOrWorst == "best"){
+		
+		//set bestFrag to 33 to check if we find an open frag
 		bestFrag = 33;
+		
+		//loop until we reach end of list
 		while(temp!= NULL){
+			
+			//if the node is free, count and continure until the node is not free
 			if (temp -> name == "free"){
 				startNodeCurrent = temp;
 				currentFrag = 0;
@@ -100,21 +149,28 @@ void linkedList::insert(string name, int programSize, string bestOrWorst){
 					temp = temp -> nextNodePage;
 				}
 			}
+			
+			//if this is the smallest found fragment, save it
 			if (programSize <= currentFrag && bestFrag > currentFrag){
 				startNodeSmallest = startNodeCurrent;
 				bestFrag = currentFrag;
 			}
+			
+			//if the temp node is not at NULL, progress to next node
 			if (temp != NULL){
 				temp = temp -> nextNodePage;
 			}
 		}
+		
+		//if a fragment has not been found or the program will not fit in it, do not add program
 		if (bestFrag == 33 || programSize > bestFrag){
-			cout << "Program " << name << " was not added successfully." << endl;
+			cout << "Program " << name << " was not added successfully.\n" << endl;
 			return;
 		}else {
-			cout << "Program " << name << " added successfully: " << programSize << " page(s) used." << endl;
+			cout << "Program " << name << " added successfully: " << programSize << " page(s) used.\n" << endl;
 		}
 		
+		//add program to all necessary pages
 		for (int x = 0; x < programSize; x++){
 			startNodeSmallest -> name = name;
 			
@@ -122,10 +178,17 @@ void linkedList::insert(string name, int programSize, string bestOrWorst){
 				startNodeSmallest = startNodeSmallest -> nextNodePage;
 			}
 		}
-		
+	
+	//if user wants worse fit algorithm
 	} else if (bestOrWorst == "worst"){
+		
+		//set bestFrag to 0 to check if we find an open fragment
 		bestFrag = 0;
+		
+		//loop until we reach end of list
 		while(temp!= NULL){
+			
+			//if the node is free, count and continure until the node is not free
 			if (temp -> name == "free"){
 				startNodeCurrent = temp;
 				currentFrag = 0;
@@ -134,21 +197,28 @@ void linkedList::insert(string name, int programSize, string bestOrWorst){
 					temp = temp -> nextNodePage;
 				}
 			}
+			
+			//if this is the largest found fragment, save it
 			if (programSize <= currentFrag && bestFrag < currentFrag){
 				startNodeSmallest = startNodeCurrent;
 				bestFrag = currentFrag;
 			}
+			
+			//if the temp node is not at NULL, progress to next node
 			if (temp != NULL){
 				temp = temp -> nextNodePage;
 			}
 		}
+		
+		//if a fragment has not been found or the program will not fit in it, do not add program
 		if (bestFrag == 0 || programSize > bestFrag){
-			cout << "Program " << name << " was not added successfully." << endl;
+			cout << "Program " << name << " was not added successfully.\n" << endl;
 			return;
 		}else {
-			cout << "Program " << name << " added successfully: " << programSize << " page(s) used." << endl;
+			cout << "Program " << name << " added successfully: " << programSize << " page(s) used.\n" << endl;
 		}
 		
+		//add program to all necessary pages
 		for (int x = 0; x < programSize; x++){
 			startNodeSmallest -> name = name;
 			
@@ -159,29 +229,47 @@ void linkedList::insert(string name, int programSize, string bestOrWorst){
 	}
 
 }
-
+/* kill function
+ @parameter string name
+ @return void
+ */
 void linkedList::kill(string name){
 	nodePage* temp = head;
+	
+	//loop to end of list
 	while (temp != NULL){
+		
+		//if a node with the name is found, set its name to "free"
 		if (temp -> name == name){
 			temp -> name = "free";
 		}
+		
+		//move to next node
 		temp = temp -> nextNodePage;
 	}
 }
-
+/* fragment counter function
+ @return int fragment
+ */
 int linkedList::countFrag(){
+	
+	//initialize temp node pointer and count int
 	nodePage* temp = head;
 	int count = 0;
 	
+	//loop till end of list
 	while (temp != NULL){
+		
+		//if we find a free node
 		if (temp -> name == "free"){
 			
+			//if we can continue, continue, if not, then return count
 			if (temp -> nextNodePage != NULL){
 				temp = temp -> nextNodePage;
 			} else {
 				return count;
 			}
+			//if we reach a node that is not free or it is pointing to null, increment count to signify the end of a fragment
 			if (temp -> name != "free" || temp -> nextNodePage == NULL){
 				count ++;
 			}
@@ -192,15 +280,26 @@ int linkedList::countFrag(){
 	return count;
 }
 
+/* check if a program is open function
+ @parameter string programName
+ @return bool existence
+ */
 bool linkedList::checkExistence(string programName){
+	
+	//initialize variables and temp node pointer
 	bool exists = false;
 	nodePage* temp = head;
 	
+	//loop through all pages
 	for (int x = 0; x < 32; x++){
+		
+		//if the program is found, set exists to true and return
 		if (temp -> name == programName){
 			exists = true;
 			return exists;
 		}
+		
+		//move to next node
 		if (temp -> nextNodePage != NULL){
 			temp = temp -> nextNodePage;
 		}
@@ -208,19 +307,25 @@ bool linkedList::checkExistence(string programName){
 	return exists;
 }
 
+/* count pages helper function for the format of killing a program
+ @parameter string programName
+ @return int number of pages
+ */
 int linkedList::countPages(string programName){
+	
+	//initialize variable and temp node pointer
 	int pages = 0;
 	nodePage* temp = head;
 	
+	//loop until we reach the end of the list
 	while (temp != NULL){
+		
+		//if a node is found with the program name, increment pages
 		if (temp -> name == programName){
 			pages++;
 		}
-		if (temp -> nextNodePage != NULL){
-			temp = temp -> nextNodePage;
-		} else {
-			return pages;
-		}
+		//move to next node
+		temp = temp -> nextNodePage;
 	}
 	return pages;
 }
